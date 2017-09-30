b0b3d95c91998db0871b6df46856aa3b04dd6a32
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2017-09-30 16:59:31 -0500

    Crea rutas y controladores para productos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :new, :create, :edit, :update]
 end
